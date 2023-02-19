65db80c6e53738e33985bddfbca824a4476ae3fb
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2023-02-19 10:48:32 -0500

    register

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const debug = require('debug');
 const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User');
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
